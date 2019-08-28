bbb49e7f2e6b56e94f70eee137b3067889640c3d
timothycote123 <20019942@hartlandschools.us>
2019-08-28 18:34:39 -0400

    fight song

---
+++ homework/src/homework/FightSong.java
@@ -0,0 +1,26 @@
+package homework;
+
+public class FightSong {
+	public static void main(String[] args) {
+		writeText();
+		writeBlank();
+		writeText();
+		writeText2();
+		writeText();
+		writeBlank();
+		writeText();
+		writeText2();
+		writeText();
+		writeBlank();
+		writeText();
+	}
+	public static void writeText() {
+		System.out.println("Go, team, go!\nYou can do it.");
+	}
+	public static void writeBlank() {
+		System.out.println();
+	}
+	public static void writeText2() {
+		System.out.println("You're the best,\nIn the West.");
+	}
+}
