444885118a5ae4985f8887a5471796eb9a4add38
timothycote123 <20019942@hartlandschools.us>
2019-08-28 19:27:27 -0400

    shining

---
+++ homework/src/homework/Shining.java
@@ -0,0 +1,43 @@
+package homework;
+/* Timothy Cote Chapter 1 Exercise 16 */
+public class Shining {
+	public static void main(String[] args) {
+		workJack1();
+		workJack1();
+		workJack1();
+		workJack1();
+		workJack1();
+		workJack1();
+		workJack1();
+		workJack1();
+		workJack1();
+		workJack1();
+	}
+	public static void workJack1() {
+		workJack2();
+		workJack2();
+		workJack2();
+		workJack2();
+		workJack2();
+		workJack2();
+		workJack2();
+		workJack2();
+		workJack2();
+		workJack2();
+	}
+	public static void workJack2() {
+		workJack3();
+		workJack3();
+		workJack3();
+		workJack3();
+		workJack3();
+		workJack3();
+		workJack3();
+		workJack3();
+		workJack3();
+		workJack3();
+	}
+	public static void workJack3() {
+		System.out.println("All work and no play makes Jack a dull boy");
+	}
+}
