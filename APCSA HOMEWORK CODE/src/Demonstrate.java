d4097c961104b6f4d12aada1f11a98e1f34fca04
timothycote123 <20019942@hartlandschools.us>
2019-08-27 20:48:38 -0400

    ap csa

---
+++ APCSA HOMEWORK CODE/src/Demonstrate.java
@@ -0,0 +1,7 @@
+public class Demonstrate {
+	public static void main(String[] args) {
+		System.out.println("This code is for github demonstration\nonly");
+		System.out.println("hopefully this works out");
+	}
+	
+}
